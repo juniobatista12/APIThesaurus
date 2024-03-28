8e326a83f69877fc9b8a1d24dae5d52803c7e10d
juniobatista12 <junio.batista12@gmail.com>
2024-03-28 18:53:19 -0300

    Endpoint /comunidades criado

--- API_2/API/app.py
+++ API_2/API/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from sqlalchemy import select
+from flask import Flask, request
+from sqlalchemy import select,create_engine
+from sqlalchemy.orm import Session
 from models import *
+import json
 
 app = Flask(__name__)
+
+def connectDb():
+    engine = create_engine("mysql+pymysql://root:avmb123@database/poc")
+    return Session(engine)
 
 @app.route("/")
 def index():
@@ -17,6 +23,22 @@ def index():
         <b>(GET) /bitstreams?id=<i>&lt;id&gt;</i>: </b>Retorna todas as informações do bitstream de id <i>id</i><br />
         <b>(DELETE) /bitstreams?id=<i>&lt;id&gt;</i>: </b>Remove o bitstream de id <i>id</i> do banco de dados
     """
+    
+@app.route("/comunidades")
+def getComunidades():
+    id = request.args.get("id", default=-1, type=int)
+    with connectDb() as session:
+        if id == -1:
+            comunidades = session.query(Comunidades)
+            resposta = []
+            for row in comunidades:
+                resposta.append({"id": row.idcomunidades, "comunidade": row.nome})
+        else:
+            colecoes = session.query(Colecoes).join(Comunidades, Comunidades.idcomunidades == Colecoes.comunidades_idcomunidades).filter(Comunidades.idcomunidades == id)
+            resposta = []
+            for row in colecoes:
+                resposta.append({"id": row.idcolecoes, "colecao": row.nome})
+    return json.dumps(resposta)
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
